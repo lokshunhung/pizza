cf051b080b754466039bb5806f61cd59e0ce90a9
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-11-14 21:44:17 +0800

    update api url

--- packages/gh-static-assets-api/pizza-service-impl.ts
+++ packages/gh-static-assets-api/pizza-service-impl.ts
@@ -4,7 +4,7 @@ import { PizzaItem, OrderItem } from "@pizza/types";
 export class PizzaServiceImpl implements PizzaService {
     async getPizzaListing(): Promise<PizzaItem[]> {
         const url =
-            "https://raw.githubusercontent.com/lokshunhung/pizza/ed0ca2afbee4efba7d7d69288c74d9198110b83e/packages/utils/fixtures/pizza-listing.json";
+            "https://raw.githubusercontent.com/lokshunhung/pizza/0cdc33c5f6e799e159444f3be69ddf639d763f8b/packages/utils/fixtures/pizza-listing.json";
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error("API call failed");

872bdc27ea366e5201b80e5c9d7c1d39e2757824
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2021-11-14 20:29:28 +0800

    image spinner fallback

--- packages/ui/AspectRatioImage/AspectRatioImage.tsx
+++ packages/ui/AspectRatioImage/AspectRatioImage.tsx
@@ -1,4 +1,5 @@
-import { AspectRatio, Box } from "@chakra-ui/react";
+import { AspectRatio, Box, Flex, Spinner } from "@chakra-ui/react";
+import { Fragment } from "react";
 
 type Props = {
     ratio: number;
@@ -9,9 +10,14 @@ export const AspectRatioImage = (props: Props) => {
     const { ratio, src } = props;
     return (
         <AspectRatio ratio={ratio}>
-            <Box position="absolute" top={0} bottom={0} left={0} right={4}>
-                <img src={src} style={{ objectFit: "fill" }} />
-            </Box>
+            <Fragment>
+                <Flex position="absolute" top={0} bottom={0} left={0} right={0}>
+                    <Spinner size="lg" />
+                </Flex>
+                <Box position="absolute" top={0} bottom={0} left={0} right={4}>
+                    <img src={src} style={{ objectFit: "fill" }} />
+                </Box>
+            </Fragment>
         </AspectRatio>
     );
 };
